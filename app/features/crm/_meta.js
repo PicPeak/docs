d12ef0383576554cadb0534581099e6fcec16cb7
Paul Nothaft <53005142+the-luap@users.noreply.github.com>
2026-09-23 18:44:58 +0200

    docs(crm): add a Customer Groups page (#35)

--- app/features/crm/_meta.js
+++ app/features/crm/_meta.js
@@ -1,6 +1,7 @@
 export default {
   index: 'Overview',
   disclaimers: 'Disclaimers',
+  'customer-groups': 'Customer Groups',
   quotes: 'Quotes',
   contracts: 'Contracts',
   invoices: 'Invoices',
